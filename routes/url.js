1fe10daec26e7ac16f7eaef3a4751e0e682d98e1
Rathod kuldeepsinh <135022358+codewithkd77@users.noreply.github.com>
2025-01-08 20:36:54 +0530

    Update url.js

--- routes/url.js
+++ routes/url.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const {handleGenerateNewShortURL} = require('../controllers/url')
 
-const router = express.Router();
+const router = express.Router();//router
 
 
 router.post('/',handleGenerateNewShortURL);
